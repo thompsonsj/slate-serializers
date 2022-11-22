1a5d239a7e9e6a32e157fff115a3f3455da6c7bc
Steven Thompson <steven@thompsonsj.com>
2022-11-22 16:25:09 +0000

    build(package.json): install and configure testing with Jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  verbose: true,
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [
+    '**/src/**/*.spec.ts',
+  ],
+}
---
+++ src/index.spec.ts
@@ -0,0 +1,4 @@
+import { Greeter } from '.'
+test('My Greeter', () => {
+  expect(Greeter('Steven')).toBe('Hello Steven')
+})
