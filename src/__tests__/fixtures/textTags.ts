4df803faca2cc841e8d10909b61c7f3608160e4d
Steven Thompson <steven@thompsonsj.com>
2022-11-24 20:12:07 +0000

    feat(slatetohtml): improve text tag support

--- src/__tests__/fixtures/textTags.ts
+++ src/__tests__/fixtures/textTags.ts
@@ -48,6 +48,67 @@ export const fixtures: Ifixture[] = [
     ],
   },
   {
+    name: "strikethrough",
+    html: "<s>Strikethrough</s>",
+    slate: [
+      {
+        children: [
+          {
+            strikethrough: true,
+            text: "Strikethrough"
+          }
+        ],
+      },
+    ],
+  },
+  {
+    name: "strong and idiomatic text",
+    html: "<strong><i>Strong and Idiomatic Text</i></strong>",
+    slate: [
+      {
+        children: [
+          {
+            bold: true,
+            italic: true,
+            text: "Strong and Idiomatic Text"
+          }
+        ],
+      },
+    ],
+  },
+  {
+    name: "strong and unarticulated annotation",
+    html: "<strong><u>Strong and Unarticulated Annotation</u></strong>",
+    slate: [
+      {
+        children: [
+          {
+            bold: true,
+            underline: true,
+            text: "Strong and Unarticulated Annotation"
+          }
+        ],
+      },
+    ],
+  },
+  /** nesting three text marks doesn't work - italic is not included */
+  /*{
+    name: "strong, unarticulated annotation and idiomatic text",
+    html: "<strong><u><i>Strong, Unarticulated Annotation and Idiomatic Text</i></u></strong>",
+    slate: [
+      {
+        children: [
+          {
+            bold: true,
+            italic: true,
+            underline: true,
+            text: "Strong, Unarticulated Annotation and Idiomatic Text"
+          }
+        ],
+      },
+    ],
+  },*/
+  {
     name: "strong in paragraph",
     html: "<p><strong>Bold</strong></p>",
     slate: [
