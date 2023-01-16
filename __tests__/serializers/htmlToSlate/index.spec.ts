f8f7367946afddeb50fbc51b6ef255ab4d887436
Steven Pribilinskiy <steven.pribilinskiy@gmail.com>
2023-01-16 14:01:06 +0100

    style: added tests folder to the prettier formatter script

--- __tests__/serializers/htmlToSlate/index.spec.ts
+++ __tests__/serializers/htmlToSlate/index.spec.ts
@@ -2,16 +2,15 @@ import { htmlToSlate, htmlToSlateConfig, payloadHtmlToSlateConfig } from '../../
 
 describe('htmlToSlate whitespace handling', () => {
   describe('inline formatting context', () => {
-
     /**
      * inline formatting example from mdn web docs
      *
      * @see https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Whitespace
-     * 
+     *
      * <h1>◦◦◦Hello◦⏎
      * ⇥⇥⇥⇥<span>◦World!</span>⇥◦◦</h1>
      */
-    it ('processes the hello world example in the same way as mdn docs', () => {
+    it('processes the hello world example in the same way as mdn docs', () => {
       const fixture = `<h1>   Hello 
         <span> World!</span>   </h1>`
       const expected: any[] = [
@@ -20,30 +19,30 @@ describe('htmlToSlate whitespace handling', () => {
             {
               text: `Hello `,
             },
-        {
-                children: [
-                  {
-                     text: " World!",
-                   },
-                 ],
-                 type: "span",
-               },
-            ],
-            "type": "h1",
-          },
-        ]
+            {
+              children: [
+                {
+                  text: ' World!',
+                },
+              ],
+              type: 'span',
+            },
+          ],
+          type: 'h1',
+        },
+      ]
       // extend the config to handle span tags
       const config = {
         ...htmlToSlateConfig,
         elementTags: {
           ...htmlToSlateConfig.elementTags,
-          span: () => ({ type: 'span' })
-        }
+          span: () => ({ type: 'span' }),
+        },
       }
       expect(htmlToSlate(fixture, config)).toEqual(expected)
     })
   })
-  
+
   describe('block formatting context', () => {
     it('processes the hello world example in the same way as mdn docs', () => {
       const fixture = `  <div>  Hello  </div>
@@ -53,35 +52,35 @@ describe('htmlToSlate whitespace handling', () => {
       const expected: any[] = [
         {
           children: [
-          {
-              text: "Hello",
+            {
+              text: 'Hello',
             },
           ],
-          type: "div",
+          type: 'div',
         },
         {
           children: [
-          {
-              text: "World!",
+            {
+              text: 'World!',
             },
           ],
-          type: "div",
+          type: 'div',
         },
         {
-            children: [
+          children: [
             {
-                text: ` . `,
-              },
-            ],
-          },
-        ]
+              text: ` . `,
+            },
+          ],
+        },
+      ]
       // extend the config to handle span tags
       const config = {
         ...htmlToSlateConfig,
         elementTags: {
           ...htmlToSlateConfig.elementTags,
-          div: () => ({ type: 'div' })
-        }
+          div: () => ({ type: 'div' }),
+        },
       }
       expect(htmlToSlate(fixture, config)).toEqual(expected)
     })
@@ -95,22 +94,21 @@ describe('htmlToSlate whitespace handling', () => {
         {
           children: [
             {
-              text: "foo",
+              text: 'foo',
             },
             {
-              text: " ",
+              text: ' ',
             },
             {
-            italic: true,
-            text: "bar",
+              italic: true,
+              text: 'bar',
             },
           ],
-          type: "p",
+          type: 'p',
         },
       ]
       expect(htmlToSlate(fixture)).toEqual(expected)
     })
-
   })
 
   describe('preserve formatting context', () => {
@@ -132,19 +130,20 @@ describe('htmlToSlate whitespace handling', () => {
     })
 
     it('removes non-mapped block elements containing whitespace only', () => {
-      const fixture = "<div>Für Ihre Sicherheit&nbsp;</div>\n<div>  </div>\n<div>Bitte beachten Sie die Ansagen der Besatzung</div>"
+      const fixture =
+        '<div>Für Ihre Sicherheit&nbsp;</div>\n<div>  </div>\n<div>Bitte beachten Sie die Ansagen der Besatzung</div>'
       const expected: any[] = [
         {
           children: [
             {
-              text: "Für Ihre Sicherheit",
+              text: 'Für Ihre Sicherheit',
             },
           ],
         },
         {
           children: [
             {
-              text: "Bitte beachten Sie die Ansagen der Besatzung",
+              text: 'Bitte beachten Sie die Ansagen der Besatzung',
             },
           ],
         },
@@ -153,19 +152,20 @@ describe('htmlToSlate whitespace handling', () => {
     })
 
     it('does not remove non-mapped block elements with text contents consisting of a non-breaking line space', () => {
-      const fixture = "<div>Für Ihre Sicherheit&nbsp;</div>\n<div>&nbsp;</div>\n<div>Bitte beachten Sie die Ansagen der Besatzung</div>"
+      const fixture =
+        '<div>Für Ihre Sicherheit&nbsp;</div>\n<div>&nbsp;</div>\n<div>Bitte beachten Sie die Ansagen der Besatzung</div>'
       const expected: any[] = [
         {
           children: [
             {
-              text: "Für Ihre Sicherheit",
+              text: 'Für Ihre Sicherheit',
             },
           ],
         },
         {
           children: [
             {
-              text: "Bitte beachten Sie die Ansagen der Besatzung",
+              text: 'Bitte beachten Sie die Ansagen der Besatzung',
             },
           ],
         },
@@ -177,73 +177,75 @@ describe('htmlToSlate whitespace handling', () => {
 
 describe('inline code and pre HTML elements', () => {
   it('can handle inline code tags', () => {
-    const html = '<p>This is editable <strong>rich</strong> text, <i>much</i> better than a <code>&lt;textarea&gt;</code>!</p>'
+    const html =
+      '<p>This is editable <strong>rich</strong> text, <i>much</i> better than a <code>&lt;textarea&gt;</code>!</p>'
     const slate = [
       {
         type: 'p',
         children: [
           {
-            text: 'This is editable '
+            text: 'This is editable ',
           },
           {
             text: 'rich',
-            bold: true
+            bold: true,
           },
           {
-            text: ' text, '
+            text: ' text, ',
           },
           {
             text: 'much',
-            italic: true
+            italic: true,
           },
           {
-            text: ' better than a '
+            text: ' better than a ',
           },
           {
             text: '<textarea>',
-            code: true
+            code: true,
           },
           {
-            text: '!'
-          }
-        ]
-      }
+            text: '!',
+          },
+        ],
+      },
     ]
     expect(htmlToSlate(html)).toEqual(slate)
   })
 
   it('can handle duplicate inline code tags', () => {
-    const html = '<p>This is editable <strong>rich</strong> text, <i>much</i> better than a <code><code>&lt;textarea&gt;</code></code>!</p>'
+    const html =
+      '<p>This is editable <strong>rich</strong> text, <i>much</i> better than a <code><code>&lt;textarea&gt;</code></code>!</p>'
     const slate = [
       {
         type: 'p',
         children: [
           {
-            text: 'This is editable '
+            text: 'This is editable ',
           },
           {
             text: 'rich',
-            bold: true
+            bold: true,
           },
           {
-            text: ' text, '
+            text: ' text, ',
           },
           {
             text: 'much',
-            italic: true
+            italic: true,
           },
           {
-            text: ' better than a '
+            text: ' better than a ',
           },
           {
             text: '<textarea>',
-            code: true
+            code: true,
           },
           {
-            text: '!'
-          }
-        ]
-      }
+            text: '!',
+          },
+        ],
+      },
     ]
     expect(htmlToSlate(html)).toEqual(slate)
   })
@@ -251,59 +253,60 @@ describe('inline code and pre HTML elements', () => {
   /**
    * Note that unlike <code> tags, <pre> tags get
    * separated out.
-   * 
+   *
    * This differs from the Slate example.
-   * 
+   *
    * Wondering if this comes from the way htmlparser2
    * translates into a DOM document object?
    */
   it('can handle inline code tags', () => {
-    const html = '<p>This is editable <strong>rich</strong> text, <i>much</i> better than a <pre>&lt;textarea&gt;</pre>!</p>'
+    const html =
+      '<p>This is editable <strong>rich</strong> text, <i>much</i> better than a <pre>&lt;textarea&gt;</pre>!</p>'
     const slate = [
       {
         type: 'p',
         children: [
           {
-            text: 'This is editable '
+            text: 'This is editable ',
           },
           {
             text: 'rich',
-            bold: true
+            bold: true,
           },
           {
-            text: ' text, '
+            text: ' text, ',
           },
           {
             text: 'much',
-            italic: true
+            italic: true,
           },
           {
-            text: ' better than a'
-          }
-        ]
+            text: ' better than a',
+          },
+        ],
       },
       {
         children: [
           {
             text: '<textarea>',
-            code: true
-          }
-        ]
-      },
-      {
-        children: [
-          {
-            text: '!'
-          }
-        ]
-      },
-      {
-        children: [
-          {
-            text: ""
-          }
+            code: true,
+          },
         ],
-        type: "p",
+      },
+      {
+        children: [
+          {
+            text: '!',
+          },
+        ],
+      },
+      {
+        children: [
+          {
+            text: '',
+          },
+        ],
+        type: 'p',
       },
     ]
     expect(htmlToSlate(html)).toEqual(slate)
@@ -316,17 +319,17 @@ describe('normalize slate JSON object', () => {
    */
   describe('ensure empty children have an empty text node', () => {
     it('adds an empty text node for an invalid paragraph', () => {
-      const html = "<p>"
+      const html = '<p>'
       const slate: any[] = [
-          {
-            children: [
-              {
-                text: ""
-              }
-            ],
-            type: "p",
-          },
-        ]
+        {
+          children: [
+            {
+              text: '',
+            },
+          ],
+          type: 'p',
+        },
+      ]
       expect(htmlToSlate(html)).toEqual(slate)
     })
   })
@@ -335,60 +338,62 @@ describe('normalize slate JSON object', () => {
 describe('empty content', () => {
   describe('ensure empty children have an empty text node', () => {
     it('adds an empty text node for an invalid paragraph', () => {
-      const html = "<p></p>"
+      const html = '<p></p>'
       const slate: any[] = [
-          {
-            children: [
-              {
-                text: ""
-              }
-            ],
-            type: "p",
-          },
-        ]
+        {
+          children: [
+            {
+              text: '',
+            },
+          ],
+          type: 'p',
+        },
+      ]
       expect(htmlToSlate(html)).toEqual(slate)
     })
 
     it('converts a br tag to a line break', () => {
-      const html = "<br />"
+      const html = '<br />'
       const slate: any[] = [
-          {
-            children:[
-              {
-                text: "\n",
-              },
-            ],
-          },
-        ]
+        {
+          children: [
+            {
+              text: '\n',
+            },
+          ],
+        },
+      ]
       expect(htmlToSlate(html)).toEqual(slate)
     })
 
     it('does nothing with a br tag if convertBrToLineBreak is false', () => {
-      const html = "<br />"
+      const html = '<br />'
       const slate: any[] = []
-      expect(htmlToSlate(html, {...htmlToSlateConfig, convertBrToLineBreak: false})).toEqual(slate)
+      expect(htmlToSlate(html, { ...htmlToSlateConfig, convertBrToLineBreak: false })).toEqual(slate)
     })
 
     it('converts a br tag to a slate node if defined as an element tag', () => {
-      const html = "<br />"
+      const html = '<br />'
       const slate: any[] = [
-          {
-            children:[
-              {
-                text: "",
-              },
-            ],
-            type: "br",
+        {
+          children: [
+            {
+              text: '',
+            },
+          ],
+          type: 'br',
+        },
+      ]
+      expect(
+        htmlToSlate(html, {
+          ...htmlToSlateConfig,
+          convertBrToLineBreak: false,
+          elementTags: {
+            ...htmlToSlateConfig.elementTags,
+            br: () => ({ type: 'br' }),
           },
-        ]
-      expect(htmlToSlate(html, {
-        ...htmlToSlateConfig,
-        convertBrToLineBreak: false,
-        elementTags: {
-          ...htmlToSlateConfig.elementTags,
-          br: () => ({type: 'br'})
-        }
-      })).toEqual(slate)
+        }),
+      ).toEqual(slate)
     })
   })
 })
--- __tests__/serializers/htmlToSlate/whitespace.spec.ts
+++ __tests__/serializers/htmlToSlate/whitespace.spec.ts
@@ -5,10 +5,12 @@ describe('processTextValue function', () => {
     it('does not do anything if the context is "preserve"', () => {
       const fixture = ` after on space
       and then line breaks and spaces`
-      expect(processTextValue({
-        text: fixture,
-        context: 'preserve'
-      })).toEqual(fixture)
+      expect(
+        processTextValue({
+          text: fixture,
+          context: 'preserve',
+        }),
+      ).toEqual(fixture)
     })
   })
 
@@ -17,30 +19,36 @@ describe('processTextValue function', () => {
     const fixture = '   text  '
     it('removes space from the start of a string if isInlineStart is true', () => {
       const expected = 'text '
-      expect(processTextValue({
-        text: fixture,
-        context: context,
-        isInlineStart: true
-      })).toEqual(expected)
+      expect(
+        processTextValue({
+          text: fixture,
+          context: context,
+          isInlineStart: true,
+        }),
+      ).toEqual(expected)
     })
 
     it('removes space from the end of a string if isInlineEnd is true', () => {
       const expected = ' text'
-      expect(processTextValue({
-        text: fixture,
-        context: context,
-        isInlineEnd: true
-      })).toEqual(expected)
+      expect(
+        processTextValue({
+          text: fixture,
+          context: context,
+          isInlineEnd: true,
+        }),
+      ).toEqual(expected)
     })
 
     it('removes space from both ends of a string if isInlineStart and isInlineEnd is true', () => {
       const expected = 'text'
-      expect(processTextValue({
-        text: fixture,
-        context: context,
-        isInlineStart: true,
-        isInlineEnd: true
-      })).toEqual(expected)
+      expect(
+        processTextValue({
+          text: fixture,
+          context: context,
+          isInlineStart: true,
+          isInlineEnd: true,
+        }),
+      ).toEqual(expected)
     })
   })
 
@@ -49,30 +57,36 @@ describe('processTextValue function', () => {
     const fixture = '   text  '
     it('normalizes space at the start of a string if isInlineStart is true', () => {
       const expected = ' text '
-      expect(processTextValue({
-        text: fixture,
-        context: context,
-        isInlineStart: true
-      })).toEqual(expected)
+      expect(
+        processTextValue({
+          text: fixture,
+          context: context,
+          isInlineStart: true,
+        }),
+      ).toEqual(expected)
     })
 
     it('normalizes space at the the end of a string if isInlineEnd is true', () => {
       const expected = ' text '
-      expect(processTextValue({
-        text: fixture,
-        context: context,
-        isInlineEnd: true
-      })).toEqual(expected)
+      expect(
+        processTextValue({
+          text: fixture,
+          context: context,
+          isInlineEnd: true,
+        }),
+      ).toEqual(expected)
     })
 
     it('normalizes space at both ends of a string if isInlineStart and isInlineEnd is true', () => {
       const expected = ' text '
-      expect(processTextValue({
-        text: fixture,
-        context: context,
-        isInlineStart: true,
-        isInlineEnd: true
-      })).toEqual(expected)
+      expect(
+        processTextValue({
+          text: fixture,
+          context: context,
+          isInlineStart: true,
+          isInlineEnd: true,
+        }),
+      ).toEqual(expected)
     })
   })
 
@@ -81,24 +95,30 @@ describe('processTextValue function', () => {
 after line break`
     const expected = 'before line break after line break'
     it('replaces line breaks with a space in an empty context', () => {
-      expect(processTextValue({
-        text: fixture,
-        context: "",
-      })).toEqual(expected)
+      expect(
+        processTextValue({
+          text: fixture,
+          context: '',
+        }),
+      ).toEqual(expected)
     })
 
     it('replaces line breaks with a space in an "inline" context', () => {
-      expect(processTextValue({
-        text: fixture,
-        context: "inline",
-      })).toEqual(expected)
+      expect(
+        processTextValue({
+          text: fixture,
+          context: 'inline',
+        }),
+      ).toEqual(expected)
     })
 
     it('replaces line breaks with a space in a "block context', () => {
-      expect(processTextValue({
-        text: fixture,
-        context: "block",
-      })).toEqual(expected)
+      expect(
+        processTextValue({
+          text: fixture,
+          context: 'block',
+        }),
+      ).toEqual(expected)
     })
   })
 })
