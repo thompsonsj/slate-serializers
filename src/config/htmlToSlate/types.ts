e253c4e66c1adeac872052cbb6435892b25c19aa
Steven Thompson <44806974+thompsonsj@users.noreply.github.com>
2023-01-18 22:07:36 +0000

    Revert "refactor(config): extend all config from a sensible default (#26)" (#27)
    This reverts commit 53a9f91fdc2b5ef5aec4872eb664b807e56bbb13.

--- src/config/htmlToSlate/types.ts
+++ src/config/htmlToSlate/types.ts
@@ -4,30 +4,18 @@ interface ItagMap {
   [key: string]: (a?: Element) => object
 }
 
-/**
- * For details on configuration options:
- * @see /docs/config/htmlToSlate.md
- */
 export interface Config {
-  /* Shortcut to map all HTML attribute/values */
   elementAttributeMap?: {
     style?: { [key: string]: string }
   }
-  /* Shortcut to map CSS attribute/values in an inline HTML style attribute */
   elementStyleMap?: {
     [key: string]: string
   }
-  /* Map HTML element tags to Slate JSON object attributes */
   elementTags: ItagMap
-  /* Map HTML text tags to Slate JSON object attributes */
   textTags: ItagMap
-  /* Perform string operations on the html string before it is parsed to a DOM Document Object Model */
   htmlPreProcessString?: (html: string) => string
-  /* Pass updater functions to modify HTML */
   htmlUpdaterMap?: HtmlUpdaterFunctionMap
-  /* Remove whitespace that does not contribute meaning */
   filterWhitespaceNodes: boolean
-  /* Convert br tags to a new line character (\n) */
   convertBrToLineBreak?: boolean
 }
 
