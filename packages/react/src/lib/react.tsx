8afd644cf94812a0b373377a83c885931ab01985
Steven Thompson <steven@thompsonsj.com>
2023-07-18 10:15:56 +0100

    fix(react): exports

--- packages/react/src/lib/react.tsx
+++ packages/react/src/lib/react.tsx
@@ -2,5 +2,5 @@
 export { SlateToReact } from './serializers'
 
 // Configuration objects
-export { Config as SlateToReactConfig } from './config/types'
+export type { Config as SlateToReactConfig } from './config/types'
 export { config as slateToReactConfig } from './config/default'
