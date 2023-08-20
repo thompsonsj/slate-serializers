549e6731320466679a572b5ff459b4fae1710f8a
Steven Thompson <steven@thompsonsj.com>
2023-08-20 14:33:12 +0100

    feat(slate-serializers): support legacy package

---
+++ packages/slate-serializers/README.md
@@ -0,0 +1,11 @@
+# slate-serializers
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build slate-serializers` to build the library.
+
+## Running unit tests
+
+Run `nx test slate-serializers` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/slate-serializers/src/lib/slate-serializers.ts
@@ -0,0 +1,19 @@
+// Serializers
+export { htmlToSlate, slateToHtml } from '@slate-serializers/html'
+export { slateToDom } from '@slate-serializers/dom'
+
+// Configuration objects
+// slateToDom
+export { SlateToDomConfig } from '@slate-serializers/dom'
+export { slateToDomConfig } from '@slate-serializers/dom'
+export { payloadSlateToDomConfig } from '@slate-serializers/dom'
+export { slateDemoSlateToDomConfig } from '@slate-serializers/dom'
+
+// htmlToSlate
+export { HtmlToSlateConfig } from '@slate-serializers/html'
+export { htmlToSlateConfig } from '@slate-serializers/html'
+export { payloadHtmlToSlateConfig } from '@slate-serializers/html'
+export { slateDemoHtmlToSlateConfig } from '@slate-serializers/html'
+
+// Useful types
+export { ElementTransform as ElementTagTransformFunction } from '@slate-serializers/dom'
