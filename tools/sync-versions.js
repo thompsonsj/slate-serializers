7250705a1d173c2e6902085f6cbd3a84686c1017
Steven Thompson <44806974+thompsonsj@users.noreply.github.com>
2023-12-11 22:39:05 +0000

    fix(react): workaround non-bundled dependency (#131)

--- tools/sync-versions.js
+++ tools/sync-versions.js
@@ -3,9 +3,17 @@ const glob = require('glob');
 const fs = require('fs');
 
 glob.sync('./packages/**/package.json')
-  .forEach(location =>
+  .forEach(location => {
+    const packageJson = JSON.parse(fs.readFileSync(location))
     fs.writeFileSync(location, JSON.stringify({
-        ...JSON.parse(fs.readFileSync(location)),
-        version: mainPackageJson.version
+        ...packageJson,
+        version: mainPackageJson.version,
+        ...(packageJson.dependencies?.['@slate-serializers/dom'] && {
+          dependencies: {
+            ...packageJson.dependencies,
+            "@slate-serializers/dom": mainPackageJson.version
+          }
+        }),
     }, null, 3))
-  );
+  }
+);
