ee00ba3f6a472a46270c4ca3e93ff7f55d883a90
Steven Thompson <44806974+thompsonsj@users.noreply.github.com>
2024-10-23 22:59:16 +0100

    test(elementmap): add table test

--- packages/html/src/lib/tests/slateToHtml/configuration/elementMap.spec.ts
+++ packages/html/src/lib/tests/slateToHtml/configuration/elementMap.spec.ts
@@ -21,4 +21,104 @@ describe("slateToHtml elementMap", () => {
     }
     expect(slateToHtml(slate, config)).toEqual(html)
   })
+
+  it('processes element map values for a table', () => {
+    const html = '<table><thead><tr><th>Heading Column 1</th><th>Heading Column 2</th></tr></thead><tbody><tr><td>Cell Row 1 Column 1</td><td>Cell Row 1 Column 2</td></tr><tbody><tr><td>Cell Row 2 Column 1</td><td>Cell Row 2 Column 2</td></tr></tbody></tbody></table>'
+    const slate = [{
+      "type": "table",
+      "children": [
+        {
+          "type": "thead",
+          "children": [
+            {
+              "type": "tr",
+              "children": [
+                {
+                  "type": "th",
+                  "children": [
+                    {
+                      "text": "Heading Column 1"
+                    }
+                  ]
+                },
+                {
+                  "type": "th",
+                  "children": [
+                    {
+                      "text": "Heading Column 2"
+                    }
+                  ]
+                },
+              ]
+            }
+          ]
+        },
+        {
+          "type": "tbody",
+          "children": [
+            {
+              "type": "tr",
+              "children": [
+                {
+                  "type": "td",
+                  "children": [
+                    {
+                      "text": "Cell Row 1 Column 1"
+                    }
+                  ]
+                },
+                {
+                  "type": "td",
+                  "children": [
+                    {
+                      "text": "Cell Row 1 Column 2"
+                    }
+                  ]
+                },
+              ]
+            },
+            {
+              "type": "tbody",
+              "children": [
+                {
+                  "type": "tr",
+                  "children": [
+                    {
+                      "type": "td",
+                      "children": [
+                        {
+                          "text": "Cell Row 2 Column 1"
+                        }
+                      ]
+                    },
+                    {
+                      "type": "td",
+                      "children": [
+                        {
+                          "text": "Cell Row 2 Column 2"
+                        }
+                      ]
+                    },
+                  ]
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    }]
+    const config = {
+      ...slateToHtmlConfig,
+      elementMap: {
+        ...slateToHtmlConfig.elementMap,
+        table: "table",
+        tr: "tr",
+        td: "td",
+        thead: "thead",
+        th: "th",
+        tbody: "tbody",
+      },
+    }
+    expect(slateToHtml(slate, config)).toEqual(html)
+  })
 })
