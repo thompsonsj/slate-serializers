1d6dc03abc18d22af68384dbecf314d5611bf35b
Steven Thompson <44806974+thompsonsj@users.noreply.github.com>
2026-04-18 12:04:51 +0100

    build(htmlparser2): dependency upgrade path 02 - htmlparser

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,25 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+/**
+ * htmlparser2 v11+ and its peer tree (domhandler, domutils, etc.) ship as ESM.
+ * Jest must transform these packages (they live under node_modules) so ts-jest can execute them.
+ */
+const esmDomStackPackages = [
+  'boolbase',
+  'css-select',
+  'css-what',
+  'dom-serializer',
+  'domelementtype',
+  'domhandler',
+  'domutils',
+  'entities',
+  'htmlparser2',
+  'nth-check',
+].join('|');
+
+module.exports = {
+  ...nxPreset,
+  transformIgnorePatterns: [
+    `/node_modules/(?!(${esmDomStackPackages})/)`,
+  ],
+};
